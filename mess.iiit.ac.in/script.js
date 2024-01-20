1fe11ed23f8539f5790e488c201dbd0b59efa1f0
SlimReaper15 <sudarshannikhil15@gmail.com>
2024-01-21 00:18:07 +0530

    Updated mess.iiit.ac.in

---
+++ mess.iiit.ac.in/script.js
@@ -0,0 +1,7 @@
+function redirectToMessRules() {
+    window.location.href = 'Attachments/Mess_Rules.pdf';
+}
+
+function redirectToBillingRates() {
+    window.location.href = 'Attachments/Billing_Rates.pdf';
+}
